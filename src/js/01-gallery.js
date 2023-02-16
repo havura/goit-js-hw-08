2bb4f592f348df9096e555e5bca40597df26570e
Havura Artur <havuraa@gmail.com>
2023-02-16 15:16:48 +0200

    update

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,29 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 
-console.log(galleryItems);
+
+import { galleryItems } from './gallery-items.js';
+
+const galerryColection = document.querySelector('.gallery');
+
+const createGalleryItems = galleryItems
+  .map(({ preview, original, description }) => {
+    return ` <div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview} "
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+  })
+    .join('');
+
+galerryColection.insertAdjacentHTML('afterbegin', createGalleryItems);
+
+new SimpleLightbox('.gallery a', {captionsData:'alt', captionDelay: 250, });
