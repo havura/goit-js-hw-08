971a74fac57a30a4b6da9528b06206e5e13575b7
Havura Artur <havuraa@gmail.com>
2023-02-16 16:59:43 +0200

    adding second task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+let CURRENT_TIME = 'videoplayer-current-time'
+
+const onPlay = function (data) {
+    localStorage.setItem(CURRENT_TIME, data.seconds)
+}
+
+player.setCurrentTime(localStorage.getItem(CURRENT_TIME));
+
+player.on('timeupdate', throttle(onPlay, 1000));
