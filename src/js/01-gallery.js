db29d800ed6840a92fa31a4ef262e1fabfd7d651
Havura Artur <havuraa@gmail.com>
2023-02-16 15:25:51 +0200

    adding first task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,11 +1,7 @@
-// Add imports above this line
+
 import { galleryItems } from './gallery-items';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
-// Change code below this line
-
-
-import { galleryItems } from './gallery-items.js';
 
 const galerryColection = document.querySelector('.gallery');
 
